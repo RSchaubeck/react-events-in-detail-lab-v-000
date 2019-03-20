657ff240a1b0f98791ba8e04d53deceb7f48e486
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-20 18:18:42 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,10 +3,6 @@ import React from 'react';
 
 class CoordinatesButton extends React.Component {
 
-  constructor(props){
-    super(props)
-  }
-
   getCoordinates = (event) => {
     this.props.onReceiveCoordinates([event.clientX, event.clietY]);
   }
